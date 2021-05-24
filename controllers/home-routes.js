3ee0016dfecddec00c7fa1008d2d998c7fe22870
cliffybar <crowellclifford@utexas.edu>
2021-05-23 21:54:16 -0500

    added basic login code in home-routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
