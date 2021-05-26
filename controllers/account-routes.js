6c494d9dbaf1901b4ec12bb2936bf6c0c9ec7b35
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-05-25 19:34:26 -0500

    account subscriptions are publishing to account.handlebars

--- controllers/account-routes.js
+++ controllers/account-routes.js
@@ -5,69 +5,42 @@ const withAuth = require('../utils/auth');
 
 // get all subscriptions for account
 router.get('/', withAuth, (req, res) => {
-  console.log(req.session);
-  console.log('======================');
-  Subscription.findAll({
-    where: {
-      user_id: req.session.user_id
-    },
-    attributes: [
-      'id',
-      'subscription_name'
-    ],
-    include: [
-      {
-        model: User,
-        attributes: ['id', 'email'],
-        include: {
-          model: UserSub,
-          attributes: ['id', 'user_id', 'subscription_id']
-        }
-      }
-    ]
-  })
-    .then(dbPostData => {
-      const subscriptions = dbPostData.map(post => post.get({ plain: true }));
-      res.render('account', { subscriptions, loggedIn: true });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.get('/edit/:id', withAuth, (req, res) => {
-  Subscription.findByPk(req.params.id, {
-    attributes: [
-        'id',
-        'subscription_name'
-      ],
+  User.findOne({
+      attributes: { exclude: ['id', 'email', 'password'] },
+      where: {
+          id: req.session.user_id
+      },
       include: [
-        {
-          model: User,
-          attributes: ['id', 'email'],
-          include: {
-            model: UserSub,
-            attributes: ['id', 'user_id', 'subscription_id']
+          {
+          model: Subscription,
+          attributes: ['subscription_name']
           }
-        }
       ]
   })
-    .then(dbPostData => {
-      if (dbPostData) {
-        const subscriptions = dbPostData.get({ plain: true });
-        
-        res.render('edit-subscriptions', {
-          subscriptions,
-          loggedIn: true
-        });
-      } else {
-        res.status(404).end();
+  .then(dbAccountData => {
+    if (!dbAccountData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
       }
+    //res.json(dbAccountData);
+    let subscriptionsArray = [];
+
+    for (i=0; i<dbAccountData.subscriptions.length; i++) {
+        const subscription = dbAccountData.subscriptions[i].subscription_name;
+        subscriptionsArray.push(subscription);
+    } 
+    console.log(subscriptionsArray);
+    res.render('account', {
+        subscriptionsArray,
+        loggedIn: req.session.loggedIn
+    });
     })
     .catch(err => {
-      res.status(500).json(err);
-    });
-});
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
 
 module.exports = router;
