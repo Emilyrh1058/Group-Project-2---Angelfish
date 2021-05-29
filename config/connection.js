a08e6dee295e7f5918c0b638ecda0c829ae15147
Emily Salinas <emilyrh1058@gmail.com>
2021-05-29 15:26:03 -0500

    .

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,24 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+// var sequelize;
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
+// }
+// module.exports = sequelize;
+
 require('dotenv').config();
-
-
-
-var sequelize;
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
+const Sequelize = require('sequelize');
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
     port: 3306
-  });
-}
+    });
 module.exports = sequelize;
