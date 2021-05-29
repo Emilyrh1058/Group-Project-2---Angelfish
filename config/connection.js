e33e95c2e6f6a7f040a32473f28631e83f27e6ca
Emily Salinas <emilyrh1058@gmail.com>
2021-05-29 14:55:32 -0500

    changed connection to read heroku

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,14 @@
 const Sequelize = require('sequelize');
-var connection;
 require('dotenv').config();
-
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Salemcaiman1619!',
-//     database: 'angelfish_db'
-//   });
-// };
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
-
-connection.connect();
+// create connection to our db
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 module.exports = sequelize;
