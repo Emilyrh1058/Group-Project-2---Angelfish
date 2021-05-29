f0265acfe32b1598d2fa7a34779f53c481358a27
Emily Salinas <emilyrh1058@gmail.com>
2021-05-28 21:23:55 -0500

    trying out a fix for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,23 @@
 const Sequelize = require('sequelize');
-
+var connection;
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Salemcaiman1619!',
+    database: 'angelfish_db'
+  });
+};
 
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   port: 3306
+// });
+
+connection.connect();
 module.exports = sequelize;
