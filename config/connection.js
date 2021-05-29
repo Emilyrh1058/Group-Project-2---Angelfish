702648f1fb69423b5ac9909753d6aa33b0fa5be3
Emily Salinas <emilyrh1058@gmail.com>
2021-05-28 22:32:53 -0500

    requesting help with keroku in office hours

--- config/connection.js
+++ config/connection.js
@@ -2,22 +2,22 @@ const Sequelize = require('sequelize');
 var connection;
 require('dotenv').config();
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Salemcaiman1619!',
-    database: 'angelfish_db'
-  });
-};
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Salemcaiman1619!',
+//     database: 'angelfish_db'
+//   });
+// };
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//   host: 'localhost',
-//   dialect: 'mysql',
-//   port: 3306
-// });
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
 
 connection.connect();
 module.exports = sequelize;
