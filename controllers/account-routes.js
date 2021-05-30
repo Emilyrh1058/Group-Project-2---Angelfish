744cab89f9296a40b765ed1bce426d198c5ae795
Emily Salinas <emilyrh1058@gmail.com>
2021-05-29 22:01:16 -0500

    fixed colors in handlebars/css and added quote

--- controllers/account-routes.js
+++ controllers/account-routes.js
@@ -1,47 +1,45 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { User, Subscription, UserSub } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { User, Subscription, UserSub } = require("../models");
+const withAuth = require("../utils/auth");
 
 // get all subscriptions for account
-router.get('/', withAuth, (req, res) => {
+router.get("/", withAuth, (req, res) => {
   User.findOne({
-      attributes: { exclude: ['id', 'email', 'password'] },
-      where: {
-          id: req.session.user_id
+    attributes: { exclude: ["id", "email", "password"] },
+    where: {
+      id: req.session.user_id,
+    },
+    include: [
+      {
+        model: Subscription,
+        attributes: ["subscription_name"],
       },
-      include: [
-          {
-          model: Subscription,
-          attributes: ['subscription_name']
-          }
-      ]
+    ],
   })
-  .then(dbAccountData => {
-    if (!dbAccountData) {
-        res.status(404).json({ message: 'No user found with this id' });
+    .then((dbAccountData) => {
+      if (!dbAccountData) {
+        res.status(404).json({ message: "No user found with this id" });
         return;
       }
-    //res.json(dbAccountData);
-    let subscriptionsArray = [];
+      //res.json(dbAccountData);
+      let subscriptionsArray = [];
 
-    for (i=0; i<dbAccountData.subscriptions.length; i++) {
+      for (i = 0; i < dbAccountData.subscriptions.length; i++) {
         const subscription = dbAccountData.subscriptions[i].subscription_name;
         subscriptionsArray.push(subscription);
-    } 
-    console.log(subscriptionsArray);
-    res.render('account', {
+      }
+      console.log(subscriptionsArray);
+      res.render("account", {
         subscriptionsArray,
         userId: req.session.user_id,
-        loggedIn: req.session.loggedIn
-    });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+        loggedIn: req.session.loggedIn,
       });
-  });
-
-
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
